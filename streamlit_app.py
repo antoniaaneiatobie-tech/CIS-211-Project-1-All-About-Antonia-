204a60a1c731805ed1b4e5b15b4e5ec1a7208a03
antoniaaneiatobie-tech <antonia.aneia.tobie@gmail.com>
2025-11-24 10:07:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,7 +145,7 @@ for skill, level in skills_data.items():
 
 
 
-  elif page == '📝 Resume':
+  elif page == "📝 Resume":
       st.title('Resume')
   # Read PDF from my GitHub repository
   with open('Antonia Tobie resume.pdf', 'rb') as pdf_file:
